551e9ea7dc1f9e6bfbaedc6dc18dc8eb308efa3c
unknown <digitone@naver.com>
2020-06-03 13:10:52 +0900

    dynamic dom check and style

--- do_type.js
+++ do_type.js
@@ -1,3 +1,8 @@
+// do type: Ver 0.2
+// 2020.06.03 by csslick
+// https://github.com/csslick/do_type
+// https://cafe.naver.com/csslick
+
 var do_type = {
 
   replay: function(_el){
@@ -19,6 +24,7 @@ var do_type = {
     var duration = 150;
     var text_count = 0;
     var type = setInterval(function(){
+    
         if(text_count >= count - 1) { 
           clearInterval(type);
           do_type.replay(_el);
@@ -27,7 +33,15 @@ var do_type = {
           el.innerHTML += '<br>';
           el_txt[text_count] = '';
         } else{
-          el.innerHTML += '<span>' + el_txt[text_count] + '</span>';  
+          var span = document.createElement('span');
+          span.innerHTML = el_txt[text_count];
+          el.append(span);
+          var all_span = document.querySelectorAll(_el + ' span');
+          for(var i = 0; i < all_span.length; i++) {
+            all_span[i].style.border = 'none';
+          }
+          var last_span = document.querySelector(_el + ' span:last-child');
+          last_span.style.borderRight = '2px solid #333'; 
         }
           text_count++;   
       }, duration)
